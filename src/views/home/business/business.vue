154364ef06532219aa12f1712872e1e6eab67c95
GuoRong <471314748@qq.com>
2020-07-25 14:01:07 +0800

    测试

--- src/views/home/business/business.vue
+++ src/views/home/business/business.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="business">
-    <h2>business企业列表</h2>
+    <el-card>
+      <el-form :inline='true' :model='form'>
+        <el-form-item label='企业标题'>
+          <el-input class="sedWidth" v-model="form.rid"></el-input>
+        </el-form-item>
+        <el-form-item label='企业名称'>
+          <el-input class="sedWidth" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label='创建者'>
+          <el-input class="sedWidth" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label='状态'>
+          <el-select placeholder="请选择状态" v-model="form.status">
+            <el-option label='启用' value='1'></el-option>
+            <el-option label='禁用' value='0'></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item labek='企业标题'>
+          <el-button type='primary'>搜索</el-button>
+          <el-button>清除</el-button>
+          <el-button type='primary'>+新增企业</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card>
+      
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      form: {
+        rid: '',
+        name: '',
+        username: '',
+        status: ''
+      }
+    }
+  },
 
 }
 </script>
 
-<style>
-
+<style lang='less' scope>
+.business{
+  .sedWidth {
+    width: 120px;
+  }
+}
 </style>
